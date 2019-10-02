dd43758ba121f74d3570285a7d11be92fcf37928
Ankhbayar <antony.baasan@gmail.com>
2019-10-01 22:58:09 -0400

    add course migration files

---
+++ b/src/main/java/com/godzula/config/dbmigrations/InitEntityMigration.java
@@ -0,0 +1,58 @@
+package com.godzula.config.dbmigrations;
+
+import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import com.godzula.domain.Course;
+import com.godzula.domain.Section;
+import com.godzula.domain.Task;
+import com.godzula.domain.enumeration.CourseStatus;
+import com.godzula.domain.enumeration.SectionStatus;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@ChangeLog(order = "002")
+public class InitEntityMigration {
+    @ChangeSet(order = "01", author = "initiator", id = "01-addCourse")
+    public void addAuthorities(MongoTemplate mongoTemplate) {
+
+        Task task = new Task();
+        task.setId("1");
+        task.setQuestion("Copy");
+        task.setAnswer("Ctrl+C");
+
+        Section section1 = new Section();
+        section1.setId("1");
+        section1.setName("Basic Excel");
+        section1.setDescription("Desc1");
+        section1.setStatus(SectionStatus.PUBLISHED);
+        section1.addTask(task);
+        task.setSection(section1);
+        mongoTemplate.save(task);
+        mongoTemplate.save(section1);
+
+        Task task2 = new Task();
+        task2.setId("2");
+        task2.setQuestion("Copy");
+        task2.setAnswer("Ctrl+C");
+
+        Section section2 = new Section();
+        section2.setId("2");
+        section2.setName("Level2 Excel");
+        section2.setDescription("Desc1");
+        section2.setStatus(SectionStatus.DRAFT);
+        section2.addTask(task2);
+        task2.setSection(section2);
+        mongoTemplate.save(task2);
+        mongoTemplate.save(section2);
+
+        Course course = new Course();
+        course.setId("1");
+        course.setName("Excel");
+        course.setDescription("This is a description field");
+        course.setStatus(CourseStatus.DRAFT);
+        course.addSection(section1);
+        course.addSection(section2);
+
+        mongoTemplate.save(course);
+    }
+}
+

04653ce3b386396407e50b754816de88042d126e
Antony Baasandorj <antony.baasan@gmail.com>
2019-10-04 23:56:38 -0400

    test commit1

--- build.sh
+++ build.sh
@@ -10,12 +10,11 @@ API_COMMIT=$(git log -1 --format=format:%H --full-diff b)
 if [ $LATEST_COMMIT = $WEB_COMMIT ];
     then
         echo "files in w has changed"
-        ./build_w.sh
-elif [ $LATEST_COMMIT = $API_COMMIT ];
+        # ./build_w.sh
+fi
+
+if [ $LATEST_COMMIT = $API_COMMIT ];
     then
         echo "files in b has changed"
-        ./build_b.sh
-else
-     echo "no folders of relevance has changed"
-     exit 0;
+        # ./build_b.sh
 fi

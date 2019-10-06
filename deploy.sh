4b0d014fbb39cf835c61d8e6abc13ba48dcaae71
Antony Baasandorj <antony.baasan@gmail.com>
2019-10-05 23:51:44 -0400

    test commit

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,15 @@ WEB_COMMIT=$(git log -1 --format=format:%H --full-diff web/)
 # latest commit where /b was changed
 API_COMMIT=$(git log -1 --format=format:%H --full-diff api/)
 
+CWD=$(pwd)
+
 if [ -z $WEB_COMMIT ]
     then
         echo "no change inside 'web' directory"
 elif [ $LATEST_COMMIT = $WEB_COMMIT ];
     then
         echo "files in 'web' has changed"
-        cd w
+        cd "$CWD/web"
         npm i -g firebase-tools
         firebase deploy --token $FIREBASE_TOKEN
 fi
@@ -24,6 +26,6 @@ if [ -z $API_COMMIT ]
 if [ $LATEST_COMMIT = $API_COMMIT ];
     then
         echo "files in 'api' has changed"
-        cd api
+        cd "$CWD/api"
         ./deploy.sh
 fi

8c55a914500350d338ec689c53ebfa682b7bbfb6
Antony Baasandorj <antony.baasan@gmail.com>
2019-12-04 23:11:13 -0500

    enable courses api for all

--- api/src/main/java/com/godzula/config/SecurityConfiguration.java
+++ api/src/main/java/com/godzula/config/SecurityConfiguration.java
@@ -97,6 +97,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/api/courses").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()

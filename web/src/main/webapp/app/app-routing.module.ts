6a5a6e7c016fba1c827426d00ae29e1ea626b41c
Antony Baasandorj <antony.baasan@gmail.com>
2020-01-31 15:54:17 -0500

    fix build issue

--- web/src/main/webapp/app/app-routing.module.ts
+++ web/src/main/webapp/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
                 },
                 {
                     path: 'admin',
-                    loadChildren: './admin/admin.module#GodzulawebAdminModule'
+                    loadChildren: () => import('./admin/admin.module').then(m => m.GodzulawebAdminModule)
                 }
             ],
             { useHash: true, enableTracing: DEBUG_INFO_ENABLED }

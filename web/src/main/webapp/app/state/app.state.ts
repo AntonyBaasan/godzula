01fa425cf57bacdd31da7c1a2dc4de4ebe27f357
Antony Baasandorj <antony.baasan@gmail.com>
2020-01-31 12:57:22 -0500

    remove unused state

--- web/src/main/webapp/app/state/app.state.ts
+++ web/src/main/webapp/app/state/app.state.ts
@@ -1,11 +1,9 @@
 import { courseReducer } from './course/course.reducer';
 import { courseDetailsReducer } from './course/course.details.reducer';
 import { sectionReducer } from './section/section.reducer';
-import { taskReducer } from './task/task.reducer';
 
 export const appReducer = {
     courses: courseReducer,
     courseDetails: courseDetailsReducer,
-    sections: sectionReducer,
-    tasks: taskReducer
+    sections: sectionReducer
 };
